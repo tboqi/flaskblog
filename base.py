05abb8576d0f00954c92c417f31b3cf931df8068
tangbq <tangbq@tiens.com>
2018-01-12 15:38:57 +0800

    update from dell msysgit

--- base.py
+++ base.py
@@ -89,8 +89,8 @@ class User(db.Model, UserMixin):
 class MyModelView(sqla.ModelView):
 
     def is_accessible(self):
-        if not current_user.is_active or not current_user.is_authenticated:
-            return False
+        if current_user.is_active and current_user.is_authenticated:
+            return True
 
         # if current_user.has_role('superuser'):
         #     return True
@@ -101,13 +101,13 @@ class MyModelView(sqla.ModelView):
         """
         Override builtin _handle_view in order to redirect users when a view is not accessible.
         """
-        # if not self.is_accessible():
-        #     if current_user.is_authenticated:
-        #         # permission denied
-        #         abort(403)
-        #     else:
-        #         # login
-        #         return redirect(url_for('security.login', next=request.url))
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
 
 
 class RoleModelView(MyModelView):
